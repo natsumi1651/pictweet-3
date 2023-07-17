b00cd18b9bb56e4f6050d439f6fecfae94ff5390
natsumi1651 <138348514+natsumi1651@users.noreply.github.com>
2023-07-18 08:13:37 +0900

    検索機能実装ずみ

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
@@ -35,6 +35,10 @@ class TweetsController < ApplicationController
     @comments = @tweet.comments.includes(:user)
   end
 
+  def search
+    @tweets = Tweet.search(params[:keyword])
+  end
+  
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
